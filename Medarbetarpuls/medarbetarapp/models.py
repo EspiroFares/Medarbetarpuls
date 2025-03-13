f95eb3d53f5e498a0d723d2a8c495c3b8ad54bfc
Eric <eric.lofgren03@gmail.com>
2025-03-13 15:05:05 +0100

    Added basic employeeGroup and link with organization

--- Medarbetarpuls/medarbetarapp/models.py
+++ Medarbetarpuls/medarbetarapp/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     # Import all classes and such here to be able to type class-fields
     from django.db.models import CharField
+    from django.db.models import IntegerField
+    from django.db.models import ForeignKey
 
 
 class Organization(models.Model):
     name: "CharField" = models.CharField(max_length=255)
 
+    # Add an explicit type hint for employeeGroups (this is just for readability)
+    employeeGroups: QuerySet["EmployeeGroup"]
+
     def __str__(self) -> str:
         return f"{self.name}"
+
+
+class EmployeeGroup(models.Model):
+    name: "CharField" = models.CharField(max_length=255)
+    organization: "ForeignKey" = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="employeeGroups"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.organization.name}"
