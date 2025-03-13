fee2403e3c30f1ef9e7f1731d9f39acae25a0b3d
Eric <eric.lofgren03@gmail.com>
2025-03-13 13:01:14 +0100

    Added basic organization class

--- Medarbetarpuls/medarbetarapp/models.py
+++ Medarbetarpuls/medarbetarapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from typing import TYPE_CHECKING
 
-# Create your models here.
+if TYPE_CHECKING:
+    # Import all classes and such here to be able to type class-fields
+    from django.db.models import CharField
+
+
+class Organization(models.Model):
+    name: "CharField" = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
