5145ef385b2515ab659a05001f5c136e182a18c0
Eric <eric.lofgren03@gmail.com>
2025-03-14 12:07:23 +0100

    Added custom user to admin panel for easier debug

--- Medarbetarpuls/medarbetarapp/admin.py
+++ Medarbetarpuls/medarbetarapp/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser  # Import your CustomUser model
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = (
+        "email",
+        "name",
+        "role",
+        "is_staff",
+        "is_superuser",
+    )  # Customize displayed fields
+    search_fields = ("email", "name")  # Add search functionality
+    list_filter = ("role", "is_staff", "is_superuser")  # Add filters
+    ordering = ("email",)  # Default sorting order
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("name", "role")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "name",
+                    "role",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+# Register the CustomUser model
+admin.site.register(CustomUser, CustomUserAdmin)
